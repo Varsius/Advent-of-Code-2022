89e5611567323aeec29e58347af19c59ddb9005d
Varsius <valentin.kuechler@online.de>
2022-12-02 17:43:26 +0100

    Cleanup

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,15 +1,20 @@
-with open("input.txt", "r") as file:
-    lines = file.readlines()
 
-calories = [0]
+def main():
+    with open("input.txt", "r") as file:
+        lines = file.readlines()
 
-for line in lines:
-    if len(line.strip()) == 0:  # Blank line --> new elf
-        calories.append(0)
-    else:
-        calories[-1] += int(line.strip())  # Add calories to last elf
+    calories = [0]
 
-calories.sort(reverse=True)  # Sort calorie values descending
+    for line in lines:
+        if len(line.strip()) == 0:  # Blank line --> new elf
+            calories.append(0)
+        else:
+            calories[-1] += int(line.strip())  # Add calories to last elf
 
-print(f"Part 1: {max(calories)} are the most calories carried by an elf.")
-print(f"Part 2: {sum(calories[:3])} calories are carried by the top three elfs.")
+    calories.sort(reverse=True)  # Sort calorie values descending
+
+    print(f"Part 1: {max(calories)} are the most calories carried by an elf.")
+    print(f"Part 2: {sum(calories[:3])} calories are carried by the top three elfs.")
+
+if __name__ == "__main__":
+    main()
