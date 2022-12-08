e842f07966ed818d65794105ac52257de1b30932
Varsius <valentin.kuechler@online.de>
2022-12-08 18:45:40 +0100

    Day 7 (complete)

--- Day 7/main.py
+++ Day 7/main.py
@@ -1,24 +1,32 @@
-def calculate_directory_size(directory, directories, directory_sizes):
+def calculate_directory_size(directory: str, directories: dict, directory_sizes: dict):
+    """
+    Calculates the total size of the given directory
+    :param directory: Path of the directory
+    :param directories: Contains the content of each directory
+    :param directory_sizes: Contains the size if each directory
+    :return: total size
+    """
 
     size = 0
 
     for content in directories[directory]:
-        if content.isnumeric():
+        if content.isnumeric():  # Files
             size += int(content)
         else:
-            size += directory_sizes[content]
+            size += directory_sizes[content]  # Other directories
 
     return size
+
 
 def main():
 
     with open("input.txt", "r") as file:
         lines = file.readlines()
 
-    directory_log = ["/"]
-    directories = {}
-    directory_levels = {"/": 1}
-    directory_sizes = {}
+    directory_log = ["/"]  # Keeps track of where we are in the directory tree
+    directories = {}  # Contents of each directory
+    directory_levels = {"/": 1}  # Keeps track how deep each directory is located in the directory tree
+    directory_sizes = {}  # Sizes of the directories
 
     for line in lines:
 
@@ -34,29 +42,34 @@ def main():
                 continue
 
             directory_log.append(destination)
-            directory_levels[destination] = len(directory_log)
+            # Since the names of the directories are not unique we have to use the full path as a key
+            directory_levels["/".join(directory_log)] = len(directory_log)  # Save how many directories we have gone up
             continue
 
         if line.startswith("dir"):
             directory_name = line.split()[1]
-            if directory_log[-1] in directories:
-                directories[directory_log[-1]].append(directory_name)
+            directory_path = "/".join(directory_log)
+            if directory_path in directories:
+                directories[directory_path].append("/".join(directory_log + [directory_name]))
             else:
-                directories[directory_log[-1]] = [directory_name]
+                directories[directory_path] = ["/".join(directory_log + [directory_name])]
             continue
 
         if line.startswith("$ ls"):
             continue
 
         file_size = line.split()[0]
-        if directory_log[-1] in directories:
-            directories[directory_log[-1]].append(file_size)
+        directory_path = "/".join(directory_log)
+        if directory_path in directories:
+            directories[directory_path].append(file_size)
         else:
-            directories[directory_log[-1]] = [file_size]
+            directories[directory_path] = [file_size]
 
     directory_names = list(directories.keys())
     while len(directory_names) > 0:
-
+        # Calculate the directories which are the furthest down in the directory tree first (do not contain any
+        # other directories which we do not know the size of yet)
+        # This is more efficient than calculating the directory sizes recursively each time
         directory_name = max(directory_names, key=directory_levels.get)
         size = calculate_directory_size(directory_name, directories, directory_sizes)
         directory_sizes[directory_name] = size
@@ -69,6 +82,18 @@ def main():
             total_sizes += size
 
     print(f"Part 1: Sum of total sizes = {total_sizes}")
+
+    free_space = 70000000 - directory_sizes["/"]
+    space_needed = 30000000 - free_space
+    smallest_sufficient_size = 30000000
+
+    for directory in directories.keys():
+        # Check if size of the directory is large enough and a better solution
+        if space_needed <= directory_sizes[directory] < smallest_sufficient_size:
+            smallest_sufficient_size = directory_sizes[directory]
+
+    print(f"Size of the smallest directory to delete = {smallest_sufficient_size}")
+
 
 if __name__ == "__main__":
     main()
